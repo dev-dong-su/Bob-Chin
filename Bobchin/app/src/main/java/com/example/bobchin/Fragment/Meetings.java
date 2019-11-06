f875763351b0bf1737c9b61db240756993612108
Younggyeong Kim <dudrud981126@naver.com>
2019-11-06 21:23:37 +0900

    C:/Program Files/Git/탭,폰트 #1 탭레이아웃,폰트 생성

---
+++ Bobchin/app/src/main/java/com/example/bobchin/Fragment/Meetings.java
@@ -0,0 +1,27 @@
+package com.example.bobchin.Fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.bobchin.R;
+
+
+public class Meetings extends Fragment {
+
+    public Meetings() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_meetings, container, false);
+    }
+}
+

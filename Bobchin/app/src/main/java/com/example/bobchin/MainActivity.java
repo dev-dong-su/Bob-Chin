681c047e16d515febfb999064f820c8dd4cd76d1
Younggyeong Kim <dudrud981126@naver.com>
2019-11-08 11:34:11 +0900

    @tab/font #2 커스텀타이틀바생성 및 내모임창 구현

--- Bobchin/app/src/main/java/com/example/bobchin/MainActivity.java
+++ Bobchin/app/src/main/java/com/example/bobchin/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.bobchin;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
 
 import com.example.bobchin.Adapter.TabPagerAdapter;
 import com.example.bobchin.Font.BaseActivity;
@@ -14,9 +20,7 @@ public class MainActivity extends BaseActivity {
     private Context mContext;
 
 
-
     private TabLayout mTabLayout;
-
 
 
     private ViewPager mViewPager;
@@ -24,10 +28,10 @@ public class MainActivity extends BaseActivity {
     private TabPagerAdapter mContentPagerAdapter;
 
 
-
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
+
 
         super.onCreate(savedInstanceState);
 
@@ -35,15 +39,13 @@ public class MainActivity extends BaseActivity {
 
         mContext = getApplicationContext();
 
-
-
         mTabLayout = (TabLayout) findViewById(R.id.layout_tab);
-        
-        mTabLayout.addTab(mTabLayout.newTab().setText("게시판"));
 
-        mTabLayout.addTab(mTabLayout.newTab().setText("내 모임"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("게시판").setIcon(R.drawable.ic_home_black_24dp));
 
-        mTabLayout.addTab(mTabLayout.newTab().setText("내 프로필"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("내 모임").setIcon(R.drawable.ic_chat_black_24dp));
+
+        mTabLayout.addTab(mTabLayout.newTab().setText("내 프로필").setIcon(R.drawable.ic_person_black_24dp));
 
         mViewPager = (ViewPager) findViewById(R.id.pager_content);
 
@@ -57,12 +59,40 @@ public class MainActivity extends BaseActivity {
             public void onTabSelected(TabLayout.Tab tab) {
                 mViewPager.setCurrentItem(tab.getPosition());
             }
+
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
             }
+
             @Override
             public void onTabReselected(TabLayout.Tab tab) {
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        ActionBar actionBar = getSupportActionBar();
+
+        // Custom Actionbar를 사용하기 위해 CustomEnabled을 true 시키고 필요 없는 것은 false 시킨다
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(false);            //액션바 아이콘을 업 네비게이션 형태로 표시합니다.
+        actionBar.setDisplayShowTitleEnabled(false);        //액션바에 표시되는 제목의 표시유무를 설정합니다.
+        actionBar.setDisplayShowHomeEnabled(false);            //홈 아이콘을 숨김처리합니다.
+
+
+        //layout을 가지고 와서 actionbar에 포팅을 시킵니다.
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View actionbar = inflater.inflate(R.layout.custom_actionbar, null);
+
+        actionBar.setCustomView(actionbar);
+
+        //액션바 양쪽 공백 없애기
+        Toolbar parent = (Toolbar) actionbar.getParent();
+        parent.setContentInsetsAbsolute(0, 0);
+        ActionBar.LayoutParams params = new ActionBar.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT,ActionBar.LayoutParams.MATCH_PARENT);
+        actionBar.setCustomView(actionbar,params);
+
+        return true;
     }
 }

6df53216222d3ed591d1b12a23099204e96957f1
김희수 <fkdlrj1230@gmail.com>
2019-11-20 18:22:58 +0900

    [login #2 로그인 엑티비티 완성]

---
+++ Bobchin/app/src/main/java/com/example/bobchin/LoginActivity.java
@@ -0,0 +1,112 @@
+package com.example.bobchin;
+
+import android.app.Application;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.MainThread;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.common.api.Scope;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.concurrent.ExecutionException;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    private GoogleSignInClient mGoogleSignInClient;
+    public static final String TAG = "ServerAuthCodeActivity";
+    private static final int RC_GET_AUTH_CODE = 9003;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        String serverClientId = getString(R.string.server_client_id);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestServerAuthCode(serverClientId)
+                .requestIdToken(serverClientId)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener((view)->{
+            onClick(view);
+        });
+    }
+
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.sign_in_button:
+                getAuthCode();
+                break;
+        }
+    }
+
+    private void getAuthCode() {
+        // Start the retrieval process for a server auth code.  If requested, ask for a refresh
+        // token.  Otherwise, only get an access token if a refresh token has been previously
+        // retrieved.  Getting a new access token for an existing grant does not require
+        // user consent.
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_GET_AUTH_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_GET_AUTH_CODE) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                String authCode = account.getServerAuthCode();
+
+                // Log.w("auth",authCode);
+
+                //AuthCode HTTP POST
+                HttpPost httpPost = new HttpPost();
+                String result = httpPost.execute("http://bobchin.cf/auth/rcvcode.php","authcode="+authCode,"loginprocess").get();
+                System.out.println(result);
+
+                //결과 ~~> JSON OBJECT 후 UserInfo 에 저장
+                BobChin bobChin = (BobChin)getApplicationContext();
+                BobChin.UserInfo userInfo = bobChin.getUserInfoObj();
+
+                JSONObject jsonObject = new JSONObject(result);
+                userInfo.setUserEmail(jsonObject.getString("email"));
+                userInfo.setUserName(jsonObject.getString("name"));
+                userInfo.setUserAccessToken(jsonObject.getString("accesstoken"));
+                userInfo.setSignedIn(true);
+
+            } catch (ApiException e) {
+                Toast.makeText(this.getApplicationContext(), "Sign-in Failed", Toast.LENGTH_LONG).show();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}

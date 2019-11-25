a6c61f77c939bc2400543063d505d8e31c7de301
cars00106 <cars00106@daum.net>
2019-11-25 21:43:53 +0900

    @map/ #1 Add Kakao Map Lib and Test Activity
    Signed-off-by: cars00106 <cars00106@daum.net>

---
+++ Bobchin/app/src/main/java/com/example/bobchin/MapTestActivity.java
@@ -0,0 +1,56 @@
+package com.example.bobchin;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.ViewGroup;
+
+import com.kakao.util.maps.helper.Utility;
+
+import net.daum.mf.map.api.MapView;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import static com.kakao.util.maps.helper.Utility.getPackageInfo;
+
+public class MapTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_test);
+
+        getHashKey(this);
+
+        MapView mapView = new MapView(this);
+
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+    }
+
+    @Nullable
+    public static String getHashKey(Context context) {
+        PackageInfo packageInfo = getPackageInfo(context, PackageManager.GET_SIGNATURES);
+        if (packageInfo == null)
+            return null;
+
+        for (Signature signature : packageInfo.signatures) {
+            try {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                return Base64.encodeToString(md.digest(), Base64.NO_WRAP);
+            } catch (NoSuchAlgorithmException e) {
+                Log.w("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+            }
+        }
+        return null;
+    }
+}

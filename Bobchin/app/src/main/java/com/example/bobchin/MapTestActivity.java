f8a9bbee1ec108197ba28a771225937a984d0866
cars00106 <cars00106@daum.net>
2019-11-28 20:34:15 +0900

    Add Marker in Map
    Signed-off-by: cars00106 <cars00106@daum.net>

--- Bobchin/app/src/main/java/com/example/bobchin/MapTestActivity.java
+++ Bobchin/app/src/main/java/com/example/bobchin/MapTestActivity.java
@@ -10,10 +10,15 @@ import android.content.pm.Signature;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.kakao.util.maps.helper.Utility;
 
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
 import java.security.MessageDigest;
@@ -21,7 +26,10 @@ import java.security.NoSuchAlgorithmException;
 
 import static com.kakao.util.maps.helper.Utility.getPackageInfo;
 
-public class MapTestActivity extends AppCompatActivity {
+public class MapTestActivity extends AppCompatActivity implements MapView.MapViewEventListener, View.OnClickListener {
+
+    MapPOIItem marker;
+    Button addMapPinButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +39,13 @@ public class MapTestActivity extends AppCompatActivity {
         getHashKey(this);
 
         MapView mapView = new MapView(this);
+        mapView.setMapViewEventListener(this);
 
         ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
         mapViewContainer.addView(mapView);
+
+        addMapPinButton = findViewById(R.id.button_add_map_pin);
+        addMapPinButton.setOnClickListener(this);
     }
 
     @Nullable
@@ -52,5 +64,57 @@ public class MapTestActivity extends AppCompatActivity {
             }
         }
         return null;
+    }
+
+    //MapViewEventListener 구현
+    @Override
+    public void onMapViewInitialized(MapView mapView) { }
+
+    @Override
+    public void onMapViewCenterPointMoved(MapView mapView, MapPoint mapPoint) {
+        if(marker != null) {
+            mapView.removePOIItem(marker);
+        }
+    }
+
+    @Override
+    public void onMapViewZoomLevelChanged(MapView mapView, int i) { }
+
+    @Override
+    public void onMapViewSingleTapped(MapView mapView, MapPoint mapPoint) {
+        marker = new MapPOIItem();
+        marker.setItemName("모임 위치");
+        marker.setTag(0);
+        marker.setMapPoint(mapPoint);
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin);
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+
+        mapView.addPOIItem(marker);
+    }
+
+    @Override
+    public void onMapViewDoubleTapped(MapView mapView, MapPoint mapPoint) { }
+
+    @Override
+    public void onMapViewLongPressed(MapView mapView, MapPoint mapPoint) { }
+
+    @Override
+    public void onMapViewDragStarted(MapView mapView, MapPoint mapPoint) { }
+
+    @Override
+    public void onMapViewDragEnded(MapView mapView, MapPoint mapPoint) { }
+
+    @Override
+    public void onMapViewMoveFinished(MapView mapView, MapPoint mapPoint) { }
+
+    //OnClickListener 구현
+    @Override
+    public void onClick(View view) {
+        if(marker == null) {
+            Toast.makeText(this, "마커를 만들어 주세요!", Toast.LENGTH_SHORT);
+        }
+        else {
+            Toast.makeText(this, "마커를 확인했어요!", Toast.LENGTH_SHORT);
+        }
     }
 }

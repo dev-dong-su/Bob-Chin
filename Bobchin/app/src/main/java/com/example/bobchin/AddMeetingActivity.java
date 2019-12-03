12152cd76c73428d712e21f5cea7f37aa6a7de33
cars00106 <cars00106@daum.net>
2019-12-03 21:08:44 +0900

    @addmeeting #9 Connet Server
    Signed-off-by: cars00106 <cars00106@daum.net>

--- Bobchin/app/src/main/java/com/example/bobchin/AddMeetingActivity.java
+++ Bobchin/app/src/main/java/com/example/bobchin/AddMeetingActivity.java
@@ -11,12 +11,29 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class AddMeetingActivity extends AppCompatActivity implements View.OnClickListener {
     double latitude;
     double longitude;
+
+    EditText titleEditText;
+    EditText maxPeopleEditText;
+    EditText contentEditText;
+
+    Spinner spinnerFromAge;
+    Spinner spinnerToAge;
+    Spinner spinnerTimeHour;
+    Spinner spinnerTimeMinute;
+    Spinner spinnerDuration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,24 +45,24 @@ public class AddMeetingActivity extends AppCompatActivity implements View.OnClic
         mapButton.setOnClickListener(this);
         findBobchinButton.setOnClickListener(this);
 
-        Spinner spinnerFromAge = findViewById(R.id.spinner_form_age);
-        Spinner spinnerToAge = findViewById(R.id.spinner_to_age);
+        spinnerFromAge = findViewById(R.id.spinner_form_age);
+        spinnerToAge = findViewById(R.id.spinner_to_age);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.age, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerFromAge.setAdapter(adapter);
         spinnerToAge.setAdapter(adapter);
 
-        Spinner spinnerSetTime = findViewById(R.id.spinner_set_time);
-        Spinner spinnerFromTime = findViewById(R.id.spinner_form_time);
-        Spinner spinnerToTime = findViewById(R.id.spinner_to_time);
+        spinnerTimeHour = findViewById(R.id.spinner_time_hour);
+        spinnerTimeMinute = findViewById(R.id.spinner_time_minute);
+        spinnerDuration = findViewById(R.id.spinner_duration);
 
-        adapter = ArrayAdapter.createFromResource(this, R.array.set_time, android.R.layout.simple_spinner_item);
-        spinnerSetTime.setAdapter(adapter);
-
-        adapter = ArrayAdapter.createFromResource(this, R.array.time, android.R.layout.simple_spinner_item);
-        spinnerFromTime.setAdapter(adapter);
-        spinnerToTime.setAdapter(adapter);
+        adapter = ArrayAdapter.createFromResource(this, R.array.hour, android.R.layout.simple_spinner_item);
+        spinnerTimeHour.setAdapter(adapter);
+        adapter = ArrayAdapter.createFromResource(this, R.array.minute, android.R.layout.simple_spinner_item);
+        spinnerTimeMinute.setAdapter(adapter);
+        adapter = ArrayAdapter.createFromResource(this, R.array.duration, android.R.layout.simple_spinner_item);
+        spinnerDuration.setAdapter(adapter);
 
         Intent intent = getIntent();
 
@@ -54,6 +71,10 @@ public class AddMeetingActivity extends AppCompatActivity implements View.OnClic
 
         TextView geoTextView = findViewById(R.id.text_geo);
         geoTextView.setText("선택한 좌표 : Latitude = " + latitude + ", Longitude = " + longitude);
+
+        titleEditText = findViewById(R.id.edit_text_title);
+        maxPeopleEditText = findViewById(R.id.edit_text_max_people);
+        contentEditText = findViewById(R.id.edit_text_content);
     }
 
     @Override
@@ -64,6 +85,45 @@ public class AddMeetingActivity extends AppCompatActivity implements View.OnClic
             finish();
         }
         else if (view.getId() == R.id.button_find_bobchin) {
+            Date currentTime = Calendar.getInstance().getTime();
+            SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy", Locale.getDefault());
+            SimpleDateFormat monthFormat = new SimpleDateFormat("mm", Locale.getDefault());
+            SimpleDateFormat dayFormat = new SimpleDateFormat("dd", Locale.getDefault());
+
+            String year = yearFormat.format(currentTime);
+            String month = monthFormat.format(currentTime);
+            String day = dayFormat.format(currentTime);
+
+            // 서버로 보낼 값들
+            String title;
+            String content;
+            int ageMax;
+            int ageMin;
+            String location;
+            String startTime;
+            double duration;
+            int maxPeople;
+
+            title = titleEditText.getText().toString();
+            content = contentEditText.getText().toString();
+            ageMax = Integer.parseInt(spinnerToAge.getSelectedItem().toString());
+            ageMin = Integer.parseInt(spinnerFromAge.getSelectedItem().toString());
+            location = longitude + ", " + latitude;
+            startTime = year +  "-" + month + "-" + day + " " + spinnerTimeHour.getSelectedItem().toString() + ":" + spinnerTimeMinute.getSelectedItem().toString() + ":00";
+            duration = Double.parseDouble(spinnerDuration.getSelectedItem().toString());
+            maxPeople = Integer.parseInt(maxPeopleEditText.getText().toString());
+
+            BobChin bobChin = (BobChin)getApplicationContext();
+            // 서버 통신
+            try {
+                HttpPost httpPost = new HttpPost();
+                String result = httpPost.execute("http://bobchin/api/addmeet.php", "token="+bobChin.getUserInfoObj().getUserAccessToken()+"&meetname="+title+"&meetmsg="+content+"&agemax="+ageMax+"&agemin="+ageMin+"&location="+location+"&starttime="+startTime+"&duration="+duration+"&maxpeople="+maxPeople).get();
+                Toast.makeText(getApplicationContext(), "결과 : " + result, Toast.LENGTH_LONG).show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            // 메인으로 돌아가는 코드
             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(intent);
             finish();
